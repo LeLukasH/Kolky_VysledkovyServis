fb013dcfe3ec6cb7c2637c0dffe47c7c8b8db8fb
LeLukasH <lukasheldak@gmail.com>
2022-12-27 13:06:22 +0100

    Create project

---
+++ kolky_vysledkovy_servis/android/app/src/main/kotlin/com/example/kolky_vysledkovy_servis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kolky_vysledkovy_servis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ kolky_vysledkovy_servis/lib/colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+MaterialColor primaryColorSwatch = const MaterialColor(
+  0xFFc12428,
+  <int, Color>{
+    50: Color(0xFFc12428),
+    100: Color(0xFFc12428),
+    200: Color(0xFFc12428),
+    300: Color(0xFFc12428),
+    400: Color(0xFFc12428),
+    500: Color(0xFFc12428),
+    600: Color(0xFFc12428),
+    700: Color(0xFFc12428),
+    800: Color(0xFFc12428),
+    900: Color(0xFFc12428),
+  },
+);
+
+MaterialColor secondaryColorSwatch = const MaterialColor(
+  0xFF0a7ec2,
+  <int, Color>{
+    50: Color(0xFF0a7ec2),
+    100: Color(0xFF0a7ec2),
+    200: Color(0xFF0a7ec2),
+    300: Color(0xFF0a7ec2),
+    400: Color(0xFF0a7ec2),
+    500: Color(0xFF0a7ec2),
+    600: Color(0xFF0a7ec2),
+    700: Color(0xFF0a7ec2),
+    800: Color(0xFF0a7ec2),
+    900: Color(0xFF0a7ec2),
+  },
+);
