452a147d0e41145aed5a12ba89cc88a47a399114
LeLukasH <lukasheldak@gmail.com>
2022-12-29 16:10:57 +0100

    Implement TabBar

--- kolky_vysledkovy_servis/lib/main.dart
+++ kolky_vysledkovy_servis/lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: primaryColorSwatch,
       ),
       home: const HomePage(),
-
     );
   }
 }
--- kolky_vysledkovy_servis/lib/screens/interliga.dart
+++ kolky_vysledkovy_servis/lib/screens/interliga.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:kolky_vysledkovy_servis/colors.dart';
 
 class Interliga extends StatelessWidget {
   const Interliga({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Interliga'),
+    const numberOfTabs = 21;
+    List<Widget> tabs = [];
+    List<Widget> tabsContent = [];
+
+    for (var i = 1; i <= numberOfTabs; i++) {
+      tabs.add(SizedBox(
+          width: 25,
+          child: Tab(
+            text: '$i.',
+          )));
+      tabsContent.add(Text('$i'));
+    }
+
+    return DefaultTabController(
+      length: numberOfTabs,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Interliga'),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: tabs,
+            indicator: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10)),
+                color: secondaryColor),
+          ),
+        ),
+        body: TabBarView(children: tabsContent),
       ),
     );
   }
